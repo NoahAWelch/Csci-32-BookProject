ba125e089c5a7cec63a99752025513d42ee18777
noah welch <noahwelch13@yahoo.com>
2024-12-16 21:11:16 -0800

    final day fixes

--- apps/csci32-nextjs-welch/src/app/Components/hideSection.tsx
+++ apps/csci32-nextjs-welch/src/app/Components/hideSection.tsx
@@ -1,35 +1,3 @@
-/*import React from 'react'
-import { Button } from '@repo/ui/button'
-import SignUp from './Books/Misc/signUp'
-import SignInForm from './Books/Misc/signInForm'
-
-export default function HideSection({ hideVisibility, isVisible }: { hideVisibility: () => void; isVisible: boolean }) {
-  return (
-    <div>
-      <div>
-        <Button onClick={hideVisibility}>{isVisible ? 'Hide Section' : 'Show Section'}</Button>
-        {isVisible && (
-          <div>
-            <SignUp
-              onUserCreated={function (userId: string): void {
-                throw new Error('Function not implemented.')
-              }}
-            />
-          </div>
-        )}
-      </div>
-      <div>
-        <Button onClick={hideLogin}>{isVisible ? 'Hide Section' : 'Show Section'}</Button>
-        {isVisible && (
-          <div>
-            <SignInForm />
-          </div>
-        )}
-      </div>
-    </div>
-  )
-}
-*/
 import React from 'react'
 import { Button } from '@repo/ui/button'
 import CreateUserForm from './Books/Misc/signUp'
