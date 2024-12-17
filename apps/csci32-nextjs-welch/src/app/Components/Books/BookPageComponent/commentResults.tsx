87bbb2b8a9e4cb25ee8df036360b54585ef60453
noah welch <noahwelch13@yahoo.com>
2024-12-16 20:25:23 -0800

    final day fixes

--- apps/csci32-nextjs-welch/src/app/Components/Books/BookPageComponent/commentResults.tsx
+++ apps/csci32-nextjs-welch/src/app/Components/Books/BookPageComponent/commentResults.tsx
@@ -1,15 +1,15 @@
-/*'use client'
+'use client'
 import { Flex } from '@repo/ui/flex'
 import { useContext } from 'react'
 import { AuthorContext } from '../../../context/authorContext'
-import CommentCard from './Comments'
+//import CommentCard from './Comments'
 
 export default function CommentResults() {
-  const { book } = useContext(AuthorContext)
+  /*const { book } = useContext(AuthorContext)
   console.log(book)
   return (
     <Flex className="gap-2 flex-wrap">
       {Array.isArray(book) && book.map(({ book_id, comment }) => <CommentCard />)}
     </Flex>
-  )
-}*/
+  )*/
+}
